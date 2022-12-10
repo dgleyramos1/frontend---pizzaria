a79383017884ae7bee09f2b9b31e701734bcf682
Dgley Ramos <dgleysilva@gmail.com>
2022-12-09 22:06:02 -0300

    Cadastrando novo usuário

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -8,6 +8,7 @@ type AuthContextData ={
     isAuthenticated: boolean;
     signIn: (credentials: SignInProps) =>Promise<void>;
     signOut: () => void;
+    signUp: (credentials: SignUpProps) => void
 }
 
 type UserProps = {
@@ -17,6 +18,12 @@ type UserProps = {
 }
 
 type SignInProps = {
+    email: string;
+    password: string;
+}
+
+type SignUpProps = {
+    name: string;
     email: string;
     password: string;
 }
@@ -78,8 +85,23 @@ export function AuthProvider({ children }: AuthProviderProps){
         }
 
     }
+
+    async function signUp({name, email, password}: SignUpProps){
+        try {
+            const response = await api.post('/users', {
+                name,
+                email,
+                password
+            })
+
+            Router.push('/');
+            
+        } catch (error) {
+            console.log("Error ao cadastrar ", error)
+        }
+    }
     return(
-        <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut}}>
+        <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut, signUp}}>
             {children}
         </AuthContext.Provider>
     )
