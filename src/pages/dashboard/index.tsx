dd6fffb3014870434ac71851f4df3a9185d62322
Dgley Ramos <dgleysilva@gmail.com>
2022-12-10 15:21:36 -0300

    Configurando o modal

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,7 +4,9 @@ import { canSSRAuth } from "../../utils/canSSRAuth"
 import styles from './styles.module.scss';
 import { FiRefreshCcw } from "react-icons/fi";
 import { setupAPIClient } from "../../services/api";
-import { useState } from "react";
+import { use, useState } from "react";
+import Modal from 'react-modal';
+import { ModalOrder } from "../../components/ModalOrder";
 
 
 type OrderProps = {
@@ -18,14 +20,53 @@ interface HomeProps{
     orders: OrderProps[];
 }
 
+export type OrderItemProps ={
+    id: string;
+    amount: number;
+    order_id: string;
+    product_id: string;
+    product: {
+        id: string;
+        name: string;
+        description: string;
+        price: string;
+        banner: string
+    }
+    order: {
+        id: string;
+        table: string | number;
+        status: boolean;
+        name: string | null;
+    }
+}
+
 export default function Dashboard({orders}: HomeProps){
 
     const [orderList, setOrderList] = useState(orders || [])
+    const [modalItem, setModalItem] = useState<OrderItemProps[]>()
+    const [modalVisible, setModalVisible] = useState(false);
 
 
-    function handleOpenModalView(id: string){
-        
+    function handleCloseModal(){
+        setModalVisible(false);
     }
+
+
+
+    async function handleOpenModalView(id: string){
+        const apiClient = setupAPIClient()
+
+        const response = await apiClient.get('/order/detail', {
+            params: {
+                order_id: id
+            }
+        })
+
+        setModalItem(response.data)
+        setModalVisible(true)
+    }
+
+    Modal.setAppElement('#__next');
 
     return (
         <>
@@ -62,6 +103,12 @@ export default function Dashboard({orders}: HomeProps){
                     </article>
 
                 </main>
+
+                {
+                    modalVisible && (
+                        <ModalOrder/>
+                    )
+                }
             </div>
         </>
     )
