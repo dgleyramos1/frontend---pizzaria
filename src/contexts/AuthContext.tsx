9200297037d8b34957869f3237bf5663d307f494
Dgley Ramos <dgleysilva@gmail.com>
2022-12-09 23:29:14 -0300

    Permanecendo usuário logado

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import {createContext, ReactNode, useState} from 'react'
+import {createContext, ReactNode, useEffect, useState} from 'react'
 import { destroyCookie, setCookie, parseCookies } from 'nookies';
 import Router from 'next/router'
 import { api } from '../services/apiClient';
@@ -50,6 +50,28 @@ export function AuthProvider({ children }: AuthProviderProps){
 
     const [user, setUser] = useState<UserProps>()
     const isAuthenticated = !!user;
+
+    useEffect(() => {
+        // tentar pegar algo no cookie
+        const { '@nextauth.token': token } = parseCookies();
+
+        if(token){
+            api.get('/me')
+            .then(response => {
+                const {id, name, email} = response.data;
+
+                setUser({
+                    id,
+                    name,
+                    email
+                })
+            })
+            .catch(() => {
+                // se deu error deslogamos o user
+                signOut();
+            })
+        }
+    }, [])
 
     async function signIn({email, password}: SignInProps){
         try {
