c49473ce331a31e54ffcb2c461cee061eb32cfe1
Dgley Ramos <dgleysilva@gmail.com>
2022-12-09 21:29:15 -0300

    Configurando a api

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,9 +1,12 @@
 import {createContext, ReactNode, useState} from 'react'
+import { destroyCookie } from 'nookies';
+import Router from 'next/router'
 
 type AuthContextData ={
     user: UserProps;
     isAuthenticated: boolean;
     signIn: (credentials: SignInProps) =>Promise<void>;
+    signOut: () => void;
 }
 
 type UserProps = {
@@ -24,18 +27,26 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData)
 
 
+export function signOut(){
+    try{
+        destroyCookie(undefined, '@nextauth.token')
+        Router.push('/')
+    }catch{
+        console.log("Error ao deslogar")
+    }
+}
+
+
 export function AuthProvider({ children }: AuthProviderProps){
 
     const [user, setUser] = useState<UserProps>()
     const isAuthenticated = !!user;
 
     async function signIn({email, password}: SignInProps){
-        console.log("Dados para logar: Email ->", email);
-        console.log("Dados para logar: Password ->", password);
-        
+
     }
     return(
-        <AuthContext.Provider value={{ user, isAuthenticated, signIn}}>
+        <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut}}>
             {children}
         </AuthContext.Provider>
     )
